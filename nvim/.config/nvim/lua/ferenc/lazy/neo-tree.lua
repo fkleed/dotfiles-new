2c8ddbe74171c1b78b954715c4b55465ae09bdc6
Ferenc Kleedoerfer <93520739+fkleed@users.noreply.github.com>
2024-12-29 13:04:05 +0100

    Updated neo-tree and none-ls

--- nvim/.config/nvim/lua/ferenc/lazy/neo-tree.lua
+++ nvim/.config/nvim/lua/ferenc/lazy/neo-tree.lua
@@ -6,21 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          ".git",
-        },
-        never_show = {},
-      },
-    },
-  },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    })
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
   end,
